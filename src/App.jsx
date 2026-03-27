e71a25b78bb2a8f580841482648b4e724fa07359
vamsi badda <vamsi.badda@artifixel.com>
2026-03-27 17:55:15 +0530

    UI Upadtes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import React from "react";
-import {  Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Home";
 import ProductList from "./Components/ProductList";
 import ProductDetails from "./Components/ProductDetails";
 
-
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<ProductList/>}/>
-        <Route path="/product/:id" element={<ProductDetails/>}/>
-      </Routes>
-      </>
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/products" element={<ProductList/>} />
+      <Route path="/product/:id" element={<ProductDetails/>} />
+    </Routes>
   );
 }
 export default App;
